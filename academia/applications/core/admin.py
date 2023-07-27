094c18d7f3fdc0918df89f6eae1a6dababc5933c
unknown <roberthbardales@hotmail.com>
2023-07-26 23:07:23 -0500

    26 07 2023

--- academia/applications/core/admin.py
+++ academia/applications/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Course
 
-# Register your models here.
+class CourseAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'teacher',
+        'class_quantity',
+                    )
+    list_filter = (
+        'teacher',
+        )
+admin.site.register(Course,CourseAdmin)
--- academia/applications/core/models.py
+++ academia/applications/core/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+# CURSOS
+
+class Course(models.Model):
+
+    name = models.CharField(max_length=90, verbose_name='Nombre')
+    description = models.TextField(blank=True, null=True, verbose_name='Descripcion')
+    teacher = models.ForeignKey(User, on_delete=models.CASCADE, limit_choices_to={'groups__name':'profesores'}, verbose_name='Profesor')
+    class_quantity=models.PositiveIntegerField(default=0,verbose_name='Cantidad de Clases')
+
+
+    def __str__(self):
+        return self.name
+    class Meta:
+
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
+
