6678d057331603e15aa4b5122ad1e2d5387db6e2
unknown <roberthbardales@hotmail.com>
2023-07-27 01:07:39 -0500

    27 07 2023

--- academia/applications/core/admin.py
+++ academia/applications/core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Course
+from .models import Course,Registration, Attendance, Mark
 
 class CourseAdmin(admin.ModelAdmin):
 
@@ -12,3 +12,50 @@ class CourseAdmin(admin.ModelAdmin):
         'teacher',
         )
 admin.site.register(Course,CourseAdmin)
+
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display = (
+        'course',
+        'student',
+        'enabled',
+                    )
+    list_filter = (
+        'course',
+        'student',
+        'enabled',
+        )
+
+admin.site.register(Registration,RegistrationAdmin)
+
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = (
+        'course',
+        'student',
+        'date',
+        'present',
+                    )
+    list_filter = (
+        'course',
+        'student',
+        'date',
+        'present',
+        )
+
+admin.site.register(Attendance,AttendanceAdmin)
+
+
+class MarkAdmin(admin.ModelAdmin):
+    list_display = (
+        'course',
+        'student',
+        'mark_1',
+        'mark_2',
+        'mark_3',
+        'average',
+                    )
+    list_filter = (
+        'course',
+        )
+
+admin.site.register(Mark,MarkAdmin)
--- academia/applications/core/models.py
+++ academia/applications/core/models.py
@@ -18,3 +18,82 @@ class Course(models.Model):
         verbose_name = 'Curso'
         verbose_name_plural = 'Cursos'
 
+# INSCRIPCIONES
+
+class Registration(models.Model):
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,verbose_name='Curso')
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='stundents_registration',
+                                limit_choices_to={'groups__name':'estudiantes'}, verbose_name='Estudiante')
+    enabled = models.BooleanField(default=True, verbose_name='Alumno Regular')
+
+    def __str__(self):
+        return f'{self.student.username}-{self.course.name}'
+
+    class Meta:
+
+        verbose_name = 'Inscripcion'
+        verbose_name_plural = 'Inscripciones'
+
+
+# Asistencias
+class Attendance(models.Model):
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,verbose_name='Curso')
+    student = models.ForeignKey(User, on_delete=models.CASCADE, related_name='attendances',
+                                limit_choices_to={'groups__name':'estudiantes'}, verbose_name='Estudiante')
+    present = models.BooleanField(default=False,blank=True,null=True,verbose_name='Presente')
+    date = models.DateField(null=True,blank=True, verbose_name='Fecha')
+
+    def __str__(self):
+        return f'Asistencia{self.id}'
+
+    # logica para generar el estado del alumno regular/ irregular(enabled)
+    # total-asistencias => class_quantity del modelo course
+    # total-inasistencias =>present=False
+    # porcentaje de inasistencias=(total-inasistencias/total-clases) *100 ----------> >20(>20%) =>alumno es irregular =>enabled = False
+    # total-clases=10
+
+
+    class Meta:
+        verbose_name = 'Asistencia'
+        verbose_name_plural = 'Asistencias'
+
+# Notas
+class Mark(models.Model):
+
+    course = models.ForeignKey(Course, on_delete=models.CASCADE,verbose_name='Curso')
+    student = models.ForeignKey(User, on_delete=models.CASCADE,
+                                limit_choices_to={'groups__name':'estudiantes'}, verbose_name='Estudiante')
+    mark_1 = models.IntegerField(null=True,blank=True, verbose_name='Nota 1')
+    mark_2 = models.IntegerField(null=True,blank=True, verbose_name='Nota 2')
+    mark_3 = models.IntegerField(null=True,blank=True, verbose_name='Nota 3')
+    average = models.DecimalField(max_digits=3, decimal_places=1,null=True,blank=True,verbose_name='Promedio')
+
+    def __str__(self):
+        return str(self.course)
+
+    class Meta:
+        verbose_name = 'Asistencia'
+        verbose_name_plural = 'Asistencia'
+
+    # Calcular el Promedio
+    def calculate_average(self):
+        marks=[self.mark_1,self.mark_2, self.mark_3]
+        valid_marks=[mark for mark in marks if mark is not None]
+        if valid_marks:
+            return sum(valid_marks)/len(valid_marks)
+        return None
+
+    def save(self,*args,**kwargs):
+        # verifico si alguna nota cambio
+        if self.mark_1 or self.mark_2 or self.mark_3:
+            self.average=self.calculate_average()       # calulcar le promedio(llamo a una funcion)
+        super().save(*args,**kwargs)
+
+    class Meta:
+        verbose_name = 'Nota'
+        verbose_name_plural = 'Notas'
+
+
+
